9fecd1d21788d08f58394d418c7ff6bb0fa07749
Ambrose Chua <ambrose@hey.com>
2020-10-31 01:22:34 +0800

    Add simplify fmt

--- main.go
+++ main.go
@@ -80,7 +80,3 @@ func index(w http.ResponseWriter, req *http.Request) {
 }
 
 
-
-
-
-
--- scripts/data.go
+++ scripts/data.go
@@ -113,3 +113,6 @@ func normalizeName(name string) string {
 func splitNames(names string) []string {
 	return strings.Split(names, ",")
 }
+
+
+
